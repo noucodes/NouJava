914ea5ff1318e29037dfe9d49716beb200a4c3ed
Nou <escudero.elton@gmail.com>
2024-04-22 20:31:09 +0800

    Changed the print statement

--- src/noujava/NouJava.java
+++ src/noujava/NouJava.java
@@ -14,7 +14,7 @@ public class NouJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hiiiiiiii!");
+        System.out.println("Hello World!");
     }
     
 }
